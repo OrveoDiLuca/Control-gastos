46c5ab1869f4ae86ab313ac089c56d33dda22be9
Orveo <orveo1902@gmail.com>
2026-02-27 17:30:37 -0400

    Se actualiza el grafico circular y los valores monetarios

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -1,37 +1,81 @@
-import  AmountDisplay from "./AmountDisplay"
+import { useMemo } from "react"
+import { useBudget } from "../hooks/useBudget"
+import AmountDisplay from "./AmountDisplay"
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 function BudgetTracker() {
-  return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap 5">
-        <div className="flex justify-center">
-            <img src="/grafico.jpg" alt="Grafico de gastos"/>
+
+    const { state } = useBudget()
+
+    const presupuesto = state.budget
+
+    const gastado = useMemo(() => (
+        state.expenses.reduce((total, expense) => expense.amount + total, 0)
+    )
+        , [state.expenses])
+
+    const disponible = useMemo(() => (
+        Math.max(state.budget - gastado, 0)
+    ), [state.expenses])
+
+    const isBudgetOverspend = useMemo(() => gastado > presupuesto, [presupuesto, gastado])
+
+    const percentage = +(gastado / presupuesto * 100).toFixed(2)
+
+    const resetApp = () => {
+        localStorage.clear()
+        window.location.reload()
+    }
+
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-2 gap 5">
+            <div className="flex justify-center">
+                <CircularProgressbar
+                    value={percentage}
+                    maxValue={100}
+                    text={`${percentage}% Gastado`}
+                    styles={buildStyles({
+                        pathColor: percentage === 100 ? '#DC2626' : '#3b82f6',
+                        trailColor: '#f5f5f5',
+                        textColor: percentage === 100 ? '#DC2626' : '#3b82f6',
+                        textSize: 8,
+                    })}
+                />
+            </div>
+
+            <div className="flex flex-col justify-center items-center gap-8">
+                <button
+                    type="button"
+                    className="bg-pink-600 w-full p-2 text-white uppercase font-bold rounded-lg hover:bg-pink-900 cursor-pointer"
+                    onClick={resetApp}
+                >
+                    Resetear App
+                </button>
+
+                <AmountDisplay
+                    label="Presupuesto"
+                    amount={presupuesto}
+                />
+
+                <AmountDisplay
+                    label="Disponible"
+                    amount={disponible}
+                />
+
+                <AmountDisplay
+                    label="Gastado"
+                    amount={gastado}
+                />
+
+                {isBudgetOverspend && (
+                    <p className="bg-red-600 text-white font-bold p-2 text-center rounded-lg w-full">
+                        ¡Has excedido el presupuesto!
+                    </p>
+                )}
+            </div>
         </div>
-
-        <div className="flex flex-col justify-center items-center gap-8">
-            <button
-                type="button"
-                className="bg-pink-600 w-full p-2 text-white uppercase font-bold rounded-lg hover:bg-pink-900 cursor-pointer"
-            >
-                Resetear App
-            </button>
-
-            <AmountDisplay
-                label="Presupuesto"
-                amount={300}
-            />
-
-            <AmountDisplay
-                label="Disponible"
-                amount={200}    
-            />
-
-            <AmountDisplay
-                label="Gastado"
-                amount={100}
-            />
-        </div>
-    </div>
-  )
+    )
 }
 
 export default BudgetTracker
